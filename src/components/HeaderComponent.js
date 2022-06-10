9bb9480a6de956041f3199ee8adb7f489a2fac95
Vaishnvee-Shinde <vaishnvee169@gmail.com>
2022-06-10 23:19:06 +0530

    first commit

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class HeaderComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <header>
+                
+                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href="#" className="navbar-brand" style={{marginLeft:"31rem"}}>React - Employee Management </a>
+                    </div>                 
+                    </nav>
+                    <div> 
+                   <img alt="emp image" src="./images/emp.png"
+                   style={{ marginTop:"0rem",marginLeft:"9rem",height: "12rem", paddingLeft: "50rem" }} 
+                   /> 
+                </div>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default HeaderComponent
---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import EmployeeService from '../services/EmployeeService'
+
+class ListEmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                employees: []
+        }
+        this.addEmployee = this.addEmployee.bind(this);
+        this.editEmployee = this.editEmployee.bind(this);
+        this.deleteEmployee = this.deleteEmployee.bind(this);
+    }
+
+    deleteEmployee(id){
+        EmployeeService.deleteEmployee(id).then( res => {
+            this.setState({employees: this.state.employees.filter(employee => employee.id !== id)});
+        });
+    }
+    viewEmployee(id){
+        this.props.history.push(`/view-employee/${id}`);
+    }
+    editEmployee(id){
+        this.props.history.push(`/add-employee/${id}`);
+    }
+
+    componentDidMount(){
+        EmployeeService.getEmployees().then((res) => {
+            this.setState({ employees: res.data});
+        });
+    }
+
+    addEmployee(){
+        this.props.history.push('/add-employee/_add');
+    }
+
+    render() {
+        return (
+            
+            <div>
+                 <h2 className="text-center"style={{ marginTop:"-10rem"}}>Working Employees List</h2>
+                 <div className = "row" style={{marginBottom:"4rem"}}>
+                    <button className="btn btn-primary" onClick={this.addEmployee}> Add Employee</button>
+                 </div>
+                 <br></br>
+                 <div className = "row" style={{marginetop:"19rem"}}>
+                        <table className = "table table-striped table-bordered">
+
+                            <thead>
+                                <tr>
+                                    <th> Employee First Name</th>
+                                    <th> Employee Last Name</th>
+                                    <th> Employee Email Id</th>
+                                    <th> Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.employees.map(
+                                        employee => 
+                                        <tr key = {employee.id}>
+                                             <td> { employee.firstName} </td>   
+                                             <td> {employee.lastName}</td>
+                                             <td> {employee.emailId}</td>
+                                             <td>
+                                                 <button onClick={ () => this.editEmployee(employee.id)} className="btn btn-info" style={{backgroundColor:"#1E90FF"}}>Update </button> {''}{''}{''}
+                                                 <button style={{marginLeft: "15px"}} onClick={ () => this.viewEmployee(employee.id)} className="btn btn-info" style={{backgroundColor:"#1E90FF"}}>View </button>
+                                                 <button style={{marginLeft: "15px"}} onClick={ () => this.deleteEmployee(employee.id)} className="btn btn-danger">Delete </button>
+                                                         </td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+
+                 </div>
+
+            </div>
+        )
+    }
+}
+
+export default ListEmployeeComponent
